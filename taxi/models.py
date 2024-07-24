77ebe8ceee62ed661d0b3849eceadeb6de919714
Sergiy Yurov <urovsergej10@gmail.com>
2024-07-24 12:35:32 +0300

    Solution

--- taxi/models.py
+++ taxi/models.py
@@ -7,7 +7,10 @@ class Manufacturer(models.Model):
     country = models.CharField(max_length=255)
 
     class Meta:
-        ordering = ['name']
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
 
 
 class Driver(AbstractUser):
@@ -20,3 +23,6 @@ class Car(models.Model):
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
     drivers = models.ManyToManyField(Driver, related_name="cars")
+
+    def __str__(self):
+        return self.model
